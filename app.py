cebd7a2d857dcb8727ae670e6f2132693979eb55
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2026-09-10 17:18:43 -0700

    Fix formatting issues in app.py

--- app.py
+++ app.py
@@ -11,5 +11,7 @@ conversation = [
       {"role": "user", "content": [
       {"type": "audio", "audio_url": "https://qianwan-res.oss-cn-beijing.aliyuncs.com/Qwen2-Audio/audio/GLASS-BREAKIN-151256.mp3},
       {"type": "text", "text"; "What's that sound?"},
+      ]},
+      {"role":"assistant", "content": "Stay Alert and cautious
       
       
