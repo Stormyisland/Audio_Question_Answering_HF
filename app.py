a9c355556530cff70bc4b1607bf4d79f5353d2dc
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2026-09-10 17:12:35 -0700

    Fix audio URL formatting in conversation list
    Fix audio URL formatting in conversation list

--- app.py
+++ app.py
@@ -9,5 +9,7 @@ model = Qwen2AudioForConditionalGeneration.from_pretrained('Qwen/Qwen2-Audio-7-I
 conversation = [
       {'role': 'system', 'comtent': 'You are a helpful assistant.'}, 
       {"role": "user", "content": [
-      {"type": "audio", "audio_url": "https://qianwan-res.oss-cn-beijing.aliyuncs.com/Qwen2-Audio/audio
+      {"type": "audio", "audio_url": "https://qianwan-res.oss-cn-beijing.aliyuncs.com/Qwen2-Audio/audio/GLASS-BREAKIN-151256.mp3},
+      {"type": "text", "text"; "What's that sound?"},
+      
       
